6700a5e6b6bb02802312be7991d1ff18b40de300
danieloliveira78 <68910464+danieloliveira78@users.noreply.github.com>
2026-03-26 14:18:54 -0300

    Modify exportar function to accept parameters

--- api.py
+++ api.py
@@ -4,9 +4,9 @@ import ExportarDados
 app = FastAPI()
 
 @app.get("/exportar")
-def exportar():
+def exportar(SELECT,WHERE,FROM,GROUP,ORDER:str):
 
-    resultado = ExportarDados.executar()
+    resultado = ExportarDados.executar(SELECT,WHERE,FROM,GROUP,ORDER:str)
 
     return {
         "status": "ok",
